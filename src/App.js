fb7f4be6bc50280e9336634045782cc58437c5f3
Sekhar Paladugu <sekharp@gmail.com>
2017-03-05 12:46:03 -0700

    initial telugu data logging

--- src/App.js
+++ src/App.js
@@ -3,9 +3,30 @@ import UnitedStatesMap from './UnitedStatesMap.js';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state =  {
+      telugu: {},
+    };
+  }
+
+  componentWillMount(){
+    fetch('http://api.census.gov/data/2013/language?get=EST,LANLABEL,NAME&for=state:06&LAN=701')
+      .then((res) => {
+        return res.json()
+      }).then((res)=>{
+        this.setState({
+          telugu: res
+        })
+      })
+  }
+
   render() {
+    console.log(this.state.telugu)
     return (
       <div className="App">
+        <h1>Indian Languages US Census Map</h1>
+        <h3><i>Data From 2009-13, US Census American Community Survey</i></h3>
         <UnitedStatesMap />
       </div>
     );
