579471976820d9a7d016430bfc951b99c4f67386
Sekhar Paladugu <sekharp@gmail.com>
2017-04-18 22:40:32 -0400

    geojson of states data seems to import properly

--- src/UnitedStatesMap.js
+++ src/UnitedStatesMap.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import geojson from './us-states.geojson';
 // import L from 'leaflet'
 
 class BaseMap extends Component {
@@ -8,8 +9,15 @@ class BaseMap extends Component {
     this.state = {
       lat: 39.742043,
       lng: -104.991531,
-      zoom: 13,
+      zoom: 2,
+      geojson: null
     };
+  }
+
+  componentDidMount() {
+    this.setState({
+      geojson
+    });
   }
 
   handleClick = (e) => {
@@ -17,6 +25,15 @@ class BaseMap extends Component {
   };
 
 // http://api.census.gov/data/2013/language?get=EST,LANLABEL,NAME&for=state:08&LAN=701
+// var mapboxAccessToken = pk.eyJ1Ijoic2VraGFycCIsImEiOiJjajFtenRxOHMwMGU0MnFuMTQ5ZGpxZnUwIn0.grcMpc9MF2c9hd4WigV_0g;
+// var map = L.map('map').setView([37.8, -96], 4);
+
+// L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + mapboxAccessToken, {
+//     id: 'mapbox.light',
+//     attribution: ...
+// }).addTo(map);
+
+// L.geoJson(statesData).addTo(map);
   render() {
     const position = [this.state.lat, this.state.lng];
     const marker = this.state.latlng
@@ -31,7 +48,7 @@ class BaseMap extends Component {
         <Map
           className="map"
           center={(this.state.latlng || [39.750809, -104.996810])}
-          zoom={20}
+          zoom={6}
           length={4}
           onClick={this.handleClick}
           onLocationfound={this.handleLocationFound}
