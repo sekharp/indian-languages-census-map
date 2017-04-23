c63d6a23da494c190faca8e6d7af5d3c317205e7
Sekhar Paladugu <sekharp@gmail.com>
2017-04-23 10:33:17 -0600

    change app header to mention telugu

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Indian Languages US Census Map</h1>
+        <h1>Indian Languages US Census Map - Telugu</h1>
         <h3><i>Data From 2009-13, US Census American Community Survey</i></h3>
         <UnitedStatesMap languageData={this.state.languageData}/>
       </div>
