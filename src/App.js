e6218298bd7124493be3e06462f1e1f3b598efbc
Sekhar Paladugu <sekharp@gmail.com>
2017-05-11 22:06:07 -0600

    dropdown change to default styling

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
       <div className="App">
         <h1>Indian Languages US Census Map</h1>
         <b style={{fontSize: 16}}>Choose Language: </b>
-        <DropdownButton bsStyle='primary' id='dropdown' title={capitalize(this.state.selectedLanguage)} onSelect={this.chooseLanguage}>
+        <DropdownButton bsStyle='default' id='dropdown' title={capitalize(this.state.selectedLanguage)} onSelect={this.chooseLanguage}>
           <MenuItem eventKey='hindi'>Hindi</MenuItem>
           <MenuItem eventKey='bengali'>Bengali</MenuItem>
           <MenuItem eventKey='panjabi'>Punjabi</MenuItem>
